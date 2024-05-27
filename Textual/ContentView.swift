82e15dc5d36f7a65cf25f391151c1c855708cca9
Henry Blanchette <henry@monkeyface.com>
2024-05-27 17:47:47 -0400

    Initial Commit

---
+++ Textual/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Textual
+//
+//  Created by Henry Blanchette on 5/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Textual/TextualApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextualApp.swift
+//  Textual
+//
+//  Created by Henry Blanchette on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TextualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
